3c264b8e3836a34714779323e827d073757d8fa9
Marco Warnke <marco.warnke@stud.h-da.de>
2019-12-05 22:41:58 +0100

    adds intcode interpreter

---
+++ intcode/intcode.py
@@ -0,0 +1,181 @@
+from typing import List
+
+
+class Instruction:
+    """
+    A class for intcode instructions, every instruction represents an executable step in an intcode program.
+    """
+
+    def __init__(self, program, raw, start, end, opcode, params, num_of_params):
+        self.opcode = opcode
+        self.num_of_params = num_of_params
+        self.params = params
+        self.raw_intcode = raw
+        # pointers into the program array
+        self.start_pos = start
+        self.end_pos = end
+        self.program = program
+
+    def __str__(self):
+        return f"[opcode: {self.opcode}, num_of_params: {self.num_of_params}, params: {str(self.params)}, raw_intcode: {self.raw_intcode}]"
+
+    def execute(self, pc) -> bool:
+        """ Executes the instruction. 
+            Has following sideeffects: 
+                updates the pc depending on the length of the executed instruction,
+                updates values in the source program.
+        """
+        if self.opcode == 1:
+            # add p1, p2, s1
+            self.params[2].save_value(
+                self.params[0].read_value() + self.params[1].read_value())
+            pc += 4
+        elif self.opcode == 2:
+            # mul p1, p2, s1
+            self.params[2].save_value(
+                self.params[0].read_value() * self.params[1].read_value())
+            pc += 4
+        elif self.opcode == 3:
+            # read p1
+            user_input = int(input("input integer: "))
+            self.params[0].save_value(user_input)
+            pc += 2
+        elif self.opcode == 4:
+            # write p1
+            print(f"output: {self.params[0].read_value()}")
+            pc += 2
+        elif self.opcode == 5:
+            # jmp_nzero p1, pc
+            if self.params[0].read_value() != 0:
+                pc = self.params[1].read_value()
+            else:
+                pc += 3
+        elif self.opcode == 6:
+            # jmp_zero p1, pc
+            if self.params[0].read_value() == 0:
+                pc = self.params[1].read_value()
+            else:
+                pc += 3
+        elif self.opcode == 7:
+            # lt p1, p2, s1
+            if self.params[0].read_value() < self.params[1].read_value():
+                self.params[2].save_value(1)
+            else:
+                self.params[2].save_value(0)
+            pc += 4
+        elif self.opcode == 8:
+            # eq p1, p2, s1
+            if self.params[0].read_value() == self.params[1].read_value():
+                self.params[2].save_value(1)
+            else:
+                self.params[2].save_value(0)
+            pc += 4
+        return pc
+
+
+class Parameter:
+    """
+    A class for intcode instruction parameters, every parameter belongs to an instruction an has a specified mode.
+    """
+
+    PARAMETER_MODES = {"position": 0, "immediate": 1}
+
+    def __init__(self, program: List[int], position: int, mode: int):
+        self.mode = mode
+        # position is an index into the source program
+        self.position = position
+        self.program = program
+
+    def __str__(self) -> str:
+        return f"[mode: {self.mode}, position: {self.position}, value: {self.read_value()}, raw_value: {self.program[self.position]}]"
+
+    def __repr__(self) -> str:
+        return self.__str__()
+
+    def read_value(self) -> int:
+        """ Returns the value of the parameter, considering the mode of the parameter """
+        if self.mode in self.PARAMETER_MODES.values():
+            if self.mode == 0:
+                return self.program[self.program[self.position]]
+            elif self.mode == 1:
+                return self.program[self.position]
+            else:
+                raise LookupError(f"Wrong mode for parameter: {self}")
+
+    def save_value(self, value: int):
+        """ Saves a value to the parameter, considering the mode of the parameter """
+        if self.mode in self.PARAMETER_MODES.values():
+            if self.mode == 0:
+                self.program[self.program[self.position]] = value
+            elif self.mode == 1:
+                self.program[self.position] = value
+            else:
+                raise LookupError(f"Wrong mode for parameter: {self}")
+
+
+class IntcodeReader:
+    """
+    A class for translating Intcode source into python objects.
+    """
+
+    def read_file(self, filename: str) -> List[int]:
+        """ Returns the intcode source as a list of integers """
+        with open(filename, "r") as f:
+            content = f.readlines()
+
+        return [int(x) for x in content[0].split(",")]
+
+    def next_instruction(self, source: List[int], pc: int) -> Instruction:
+        """ Reads the next instruction following the pc. Does not change the pc. """
+        full_instr_code = str(source[pc]).zfill(5)
+        opcode = int(full_instr_code[3:])
+        param3_mode = int(full_instr_code[:1])
+        param2_mode = int(full_instr_code[1:2])
+        param1_mode = int(full_instr_code[2:3])
+        param_modes = [param1_mode, param2_mode, param3_mode]
+
+        if opcode in [1, 2, 7, 8]:
+            # three param instructions
+            length = 3
+        elif opcode in [5, 6]:
+            # two param instructions
+            length = 2
+        elif opcode in [3, 4]:
+            # single param instrcutions
+            length = 1
+        elif opcode == 99:
+            return None
+        else:
+            raise LookupError(
+                f"Could not create instruction for opcode: {opcode}")
+
+        params = [Parameter(source, pc + i[0], i[1])
+                  for i in zip(range(1, length + 1), param_modes)]
+        return Instruction(
+            source, source[pc:pc + length + 1], pc, pc + length + 1, opcode, params, length)
+
+
+class IntcodeInterpreter:
+    """
+    A class which can execute Intcode from files or lists.
+    """
+
+    def __init__(self):
+        self.reader = IntcodeReader()
+        self.pc = 0
+
+    def execute_file(self, filename: str):
+        source = self.reader.read_file(filename)
+        self.execute(source)
+
+    def execute(self, source: List[int]):
+        while True:
+            instruction = self.reader.next_instruction(source, self.pc)
+            if instruction is None:
+                break
+            else:
+                self.pc = instruction.execute(self.pc)
+
+
+interpreter = IntcodeInterpreter()
+interpreter.execute_file("d:/aoc/day05/input.txt")
